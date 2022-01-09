1e5dfc479a4654e3e04117b9f2a35f8d0f6c38dd
Lev <l.goryachev@inbox.ru>
2022-01-09 23:47:32 +0300

    ProxyServices Integration Tests (+4 items)

--- MCEngineeringFunctionOrchestrator/src/test/java/ru/goryachev/multichief/orchestras/engineeringfunctionorchestrator/service/implementation/ProxyEstimateServiceIntegrationTest.java
+++ MCEngineeringFunctionOrchestrator/src/test/java/ru/goryachev/multichief/orchestras/engineeringfunctionorchestrator/service/implementation/ProxyEstimateServiceIntegrationTest.java
@@ -1,8 +1,8 @@
 package ru.goryachev.multichief.orchestras.engineeringfunctionorchestrator.service.implementation;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.*;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * Integration testing with real DB.
@@ -30,33 +30,29 @@ import static org.junit.jupiter.api.Assertions.*;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @SpringBootTest(classes = WebAppInit.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-public class ProxyBimServiceIntegrationTest {
+public class ProxyEstimateServiceIntegrationTest {
 
     private Long testId;
 
-    private final Logger log = LoggerFactory.getLogger(ProxyBimServiceIntegrationTest.class);
+    private final Logger log = LoggerFactory.getLogger(ProxyEstimateServiceIntegrationTest.class);
 
     @Autowired
-    private ProxyBimService proxyBimService;
+    private ProxyEstimateService proxyEstimateService;
 
     @Test
     @Order(1)
     @Rollback(value = false)
-    public void saveBimTest(){
-        Map<String, Object> newBim = new LinkedHashMap<>();
-        newBim.put("projectCodeNumber","TestValueAbc");
-        newBim.put("projectName","Test Value DEF");
-        newBim.put("lod","500");
-        newBim.put("projectTypeId","");
-        newBim.put("eirId","");
-        newBim.put("link","TestValueXyz");
-        Object microserviceResponse = proxyBimService.save(newBim);
+    public void saveEstimateTest(){
+        Map<String, Object> newEstimate = new LinkedHashMap<>();
+        newEstimate.put("estimateCodeNumber","TestValueAbc");
+        newEstimate.put("link","TestValueD");
+        Object microserviceResponse = proxyEstimateService.save(newEstimate);
         ObjectMapper objectMapper = new ObjectMapper();
         Map<Object, Object> responseMap = objectMapper.convertValue(microserviceResponse, Map.class);
         this.testId = Long.parseLong(responseMap.get("id").toString());
 
         if (log.isDebugEnabled()) {
-            log.debug("ProxyBimService" + ", " + "save():" + " " + responseMap.toString());
+            log.debug("ProxyEstimateService" + ", " + "save():" + " " + responseMap.toString());
         }
 
         Assertions.assertThat(this.testId).isGreaterThan(0);
@@ -64,8 +60,8 @@ public class ProxyBimServiceIntegrationTest {
 
     @Test
     @Order(2)
-    public void getBimByIdTest(){
-        Object microserviceResponse = proxyBimService.getOne(testId);
+    public void getEstimateByIdTest(){
+        Object microserviceResponse = proxyEstimateService.getOne(testId);
         ObjectMapper objectMapper = new ObjectMapper();
         Map<Object, Object> dtoMap = objectMapper.convertValue(microserviceResponse, Map.class);
 
@@ -74,8 +70,8 @@ public class ProxyBimServiceIntegrationTest {
 
     @Test
     @Order(3)
-    public void getAllBimsTest(){
-        List<Object> microserviceResponse = proxyBimService.getAll();
+    public void getAllEstimatesTest(){
+        List<Object> microserviceResponse = proxyEstimateService.getAll();
         ObjectMapper objectMapper = new ObjectMapper();
         List<Map<Object, Object>> dtoMaps = microserviceResponse.stream().map(n ->(Map<Object, Object>) objectMapper.convertValue(n, Map.class)).collect(Collectors.toList());
 
@@ -85,33 +81,33 @@ public class ProxyBimServiceIntegrationTest {
     @Test
     @Order(4)
     @Rollback(value = false)
-    public void updateBimTest(){
+    public void updateEstimateTest(){
         ObjectMapper objectMapper = new ObjectMapper();
 
-        Object oldDto = proxyBimService.getOne(testId);
+        Object oldDto = proxyEstimateService.getOne(testId);
         Map<String, Object> oldDtoMap = objectMapper.convertValue(oldDto, Map.class);
-        oldDtoMap.put("projectName","Updated test Value");
+        oldDtoMap.put("estimateCodeNumber","Updated test Value");
 
-        Object dtoUpdated = proxyBimService.save(oldDtoMap);
+        Object dtoUpdated = proxyEstimateService.save(oldDtoMap);
 
         if (log.isDebugEnabled()) {
-            log.debug("ProxyBimService" + ", " + "update():" + " " + dtoUpdated.toString());
+            log.debug("ProxyEstimateService" + ", " + "update():" + " " + dtoUpdated.toString());
         }
 
-        Object microserviceResponse = proxyBimService.getOne(testId);
+        Object microserviceResponse = proxyEstimateService.getOne(testId);
         Map<Object, Object> dtoUpdatedReaponse = objectMapper.convertValue(microserviceResponse, Map.class);
 
-        Assertions.assertThat(dtoUpdatedReaponse.get("projectName")).isEqualTo("Updated test Value");
+        Assertions.assertThat(dtoUpdatedReaponse.get("estimateCodeNumber")).isEqualTo("Updated test Value");
     }
     @Test
     @Order(5)
     @Rollback(value = false)
-    public void deleteBimTest(){
+    public void deleteEstimateTest(){
 
-        Object microserviceResponse = proxyBimService.delete(testId);
+        Object microserviceResponse = proxyEstimateService.delete(testId);
         if (log.isDebugEnabled()) {
-            log.debug("ProxyBimService" + ", " + "delete():" + " " + microserviceResponse);
+            log.debug("ProxyEstimateService" + ", " + "delete():" + " " + microserviceResponse);
         }
-        assertThrows(Exception.class, ()->{proxyBimService.getOne(testId);} );
+        assertThrows(Exception.class, ()->{proxyEstimateService.getOne(testId);} );
     }
 }
